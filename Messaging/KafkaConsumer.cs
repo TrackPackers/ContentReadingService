5d851ccc036042812bfa62d165295bdfc36c446e
BartVnim <65620353+BartvNim@users.noreply.github.com>
2023-03-28 12:05:14 +0200

    add Kafka Reader

---
+++ Messaging/KafkaConsumer.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using System.Text;
+using System.Text.Unicode;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+
+
+namespace ContentReaderService
+{
+    public class KafkaConsumer : IHostedService
+    {
+        private readonly ILogger<KafkaConsumer> _logger;
+        private ClusterClient _cluster;
+
+        public KafkaConsumer(ILogger<KafkaConsumer> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest("NEW_CONTENT");
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation("NEW CONTENT: " + Encoding.UTF8.GetString(record.Value as byte[]));
+            };
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster.Dispose();
+            return Task.CompletedTask;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using ContentReaderService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+{
+    services.AddHostedService<KafkaConsumer>();
+}).Build().RunAsync();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 

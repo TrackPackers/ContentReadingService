b315a36fc0996e3ce19b82a6d87200145e98b124
BartVnim <65620353+BartvNim@users.noreply.github.com>
2023-04-11 21:02:27 +0200

    add Redis

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Text.Json.Nodes;
+
+namespace newPostsFeed.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContentController : ControllerBase
+    {
+        private readonly IDatabase _redisDatabase;
+        public ContentController()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _redisDatabase = redis.GetDatabase();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var hashEntriesMessages = _redisDatabase.HashGetAll("Message");
+
+            var jsonArray = new JsonArray();
+            foreach (var obj in hashEntriesMessages)
+            {
+                var objectid = obj.Name.ToString();
+                var message = obj.Value.ToString();
+                var name = _redisDatabase.HashGet("Name", objectid);
+
+                var jsonObject = new JsonObject();
+                jsonObject.Add("ObjectID", objectid);
+                jsonObject.Add("message", message);
+                jsonObject.Add("name", name.ToString());
+
+                jsonArray.Add(jsonObject);
+            }
+
+            return Ok(jsonArray.ToString());
+        }
+    }
+}
--- Messaging/KafkaConsumer.cs
+++ Messaging/KafkaConsumer.cs
@@ -3,7 +3,10 @@ using System.Text;
 using System.Text.Unicode;
 using Kafka.Public;
 using Kafka.Public.Loggers;
-
+using StackExchange.Redis;
+using System.Text.Json.Serialization;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json.Nodes;
 
 namespace newPostsFeed
 {
@@ -11,10 +14,16 @@ namespace newPostsFeed
     {
         private readonly ILogger<KafkaConsumer> _logger;
         private ClusterClient _cluster;
+        private readonly IDatabase _redisDatabase;
+
 
         public KafkaConsumer(ILogger<KafkaConsumer> logger)
         {
             _logger = logger;
+
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _redisDatabase = redis.GetDatabase();
+
             _cluster = new ClusterClient(new Configuration
             {
                 Seeds = "localhost:9092"
@@ -26,7 +35,27 @@ namespace newPostsFeed
             _cluster.ConsumeFromLatest("NEW_CONTENT");
             _cluster.MessageReceived += record =>
             {
-                _logger.LogInformation("NEW CONTENT: " + Encoding.UTF8.GetString(record.Value as byte[]));
+                var json = JsonObject.Parse(Encoding.UTF8.GetString(record.Value as byte[]));
+                var id = (string)json["Id"];
+                var name = (string)json["Name"];
+                var message = (string)json["Message"];
+                var createdAt = (string)json["CreatedAt"];
+
+                _logger.LogInformation(createdAt.ToString());
+
+                _redisDatabase.HashSet("Message", new[]
+                {
+                    new HashEntry(id, message),
+                });
+                _redisDatabase.HashSet("Name", new[]
+{
+                    new HashEntry(id, name),
+                });
+                _redisDatabase.HashSet("CreatedAt", new[]
+{
+                    new HashEntry(id, createdAt),
+                });
+
             };
             return Task.CompletedTask;
         }
